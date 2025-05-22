548c7c4119067ac34952079d719f358090667faf
shahrazkclev <cgmathv99@gmail.com>
2025-05-22 19:33:32 +0500

    Update check-verification.js

--- api/check-verification.js
+++ api/check-verification.js
@@ -3,9 +3,11 @@
 global.verificationStorage = global.verificationStorage || new Map();
 
 export default async function handler(req, res) {
+  // Enhanced CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();

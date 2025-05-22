4fea65175701f2960da272c808222d98236d3085
shahrazkclev <cgmathv99@gmail.com>
2025-05-22 19:34:02 +0500

    Update store-bundle.js

--- api/store-bundle.js
+++ api/store-bundle.js
@@ -4,10 +4,11 @@ global.tokenStorage = global.tokenStorage || new Map();
 global.verificationStorage = global.verificationStorage || new Map();
 
 export default async function handler(req, res) {
-  // Enable CORS
+  // Enhanced CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
